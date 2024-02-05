cc96f9e6b49a252a8b602f1cee1cdd68a48fe5fe
AlmasJakeyev <a_jakeyev@kbtu.kz>
2024-02-05 22:50:01 +0600

    task12

--- lecture3/functions1/task12.py
+++ lecture3/functions1/task12.py
@@ -1,15 +1,8 @@
-import random
+def histogramma(x):
 
-def guess(y):
-    x = random.randint(1, 5)
-    count = 1
-    while y != x:
-        print("Your guess is too low.\nTake a guess") 
-        y = int(input())
-        count += 1
-    print("Good job, KBTU! You guessed my number in", count, "guesses!")
+    for i in x:
+        print('*'*i, end = '\n')
 
-number = int(input())
+x = [ x for x in map(int, input().split())]
 
-guess(number)
-
+histogramma(x)
